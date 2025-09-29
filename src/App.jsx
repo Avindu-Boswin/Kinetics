01b9da2e169ea7b6b9097a562b45afd518205ab4
Avindu Payagalage <avindu.k@boswingroup.com>
2025-09-29 06:46:19 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <>
+      <h1 className='text-blue-500 text-6xl'>Kinetics</h1>
+    </>
+  )
+}
+
+export default App
